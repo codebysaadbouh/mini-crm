625f546f3c320fb32d77b48bafd09949dcf37aa4
Cheikh Saad Bouh SOW <saadbouh.code@gmail.com>
2022-05-08 17:09:00 +0200

    Initialisation d'API Platform, mettre en place les annotations @ApiResource() au niveau de nos entités

--- src/Entity/Invoice.php
+++ src/Entity/Invoice.php
@@ -2,11 +2,13 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Core\Annotation\ApiResource;
 use App\Repository\InvoiceRepository;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ORM\Entity(repositoryClass=InvoiceRepository::class)
+ * @ApiResource()
  */
 class Invoice
 {
